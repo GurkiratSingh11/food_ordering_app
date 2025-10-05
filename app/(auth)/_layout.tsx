4c194de3e4c65c2436b8155d85c38dc535f6d69b
GurkiratSingh11 <guriaulakh117@gmail.com>
2025-10-05 18:25:55 +0530

    Implemented full auth and tabs navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,17 +1,34 @@
-import { Text, View } from "react-native";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Redirect, Slot } from "expo-router";
+import {View, Text, KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image} from 'react-native'
+import {Redirect, Slot} from "expo-router";
+import {images} from "@/constants";
+import useAuthStore from '@/store/auth.store';
 
 
 export default function AuthLayout() {
-          const isAuthenticated = false; 
+    const { isAuthenticated } = useAuthStore();
+    console.log('isAuthenticated',isAuthenticated);
+    
 
     if(isAuthenticated) return <Redirect href="/" />
     return (
-        <SafeAreaView>
-            <Text>Auth Layout</Text>
-            <Slot/>
-        </SafeAreaView>
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+                <View className="w-full relative" style={{ height: Dimensions.get('screen').height / 2.25}}>
+                    <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode="stretch" />
+                    <Image source={images.logo} className="self-center size-48 absolute -bottom-16 z-10" />
+                </View>
+              
+              {/* <CustomInput
+                placeholder="Enter your email"
+                value={''}
+                onChangeText={(text) => {}}
+                label="Email"
+                keyboardType="email-address"
+            />
+              <CustomButton/> */}
+              
+              <Slot />
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
